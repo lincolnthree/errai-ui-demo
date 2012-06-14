db694fee8f09b93e611d387414ab171c8271393a
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2012-06-14 13:48:45 -0400

    Removed Configuration for initial state

--- src/main/java/org/jboss/errai/ui/demo/client/shared/Profile.java
+++ src/main/java/org/jboss/errai/ui/demo/client/shared/Profile.java
@@ -1,9 +1,7 @@
 package org.jboss.errai.ui.demo.client.shared;
 
 import org.jboss.errai.common.client.api.annotations.Portable;
-import org.jboss.errai.databinding.client.api.Bindable;
 
-@Bindable
 @Portable
 public class Profile
 {
